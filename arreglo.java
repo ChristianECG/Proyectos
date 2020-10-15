3618effa534a5491edcb896cd641c0dfae127cf7
Osmar <cossmb7@gmail.com>
2020-10-15 16:59:56 -0500

    Primer bloque de codigo

---
+++ arreglo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/
+ *
+ * @author x
+ */
+public class Rellenar {
+
+    /
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int elementos= Integer.parseInt(JOptionPane.showInputDialog("Ingresar numero de elementos"));
+        Scanner sc= new Scanner(System.in);
+        char[] letras = new char[elementos];
+        //Solicitar
+
+        System.out.println("Digite los elementos del arreglo: ");
+        for(int i=0;i<elementos;i++){
+            System.out.print((i+1)+". Digite un caracter:");
+            letras[i] = sc.next().charAt(0);//Guardar primer caracter --charArt
+        }
+        //Mostrar
+        }
+    }
+
+}
